c1384a4c5cbbb17e12926fbc63fa62599ad54ff9
hongweizhu.com <root@hongweizhu.com>
2022-01-16 14:42:46 +0800

    A B A+B

--- main.ts
+++ main.ts
@@ -1,132 +1,14 @@
-input.onButtonPressed(Button.A, function () {
-    basic.clearScreen()
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(1000*60*5)
-    basic.showLeds(`
-        # # # # #
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(1000*60*5)
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(1000*60*5)
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(1000*60*5)
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        . . . . .
-        `)
-    basic.pause(1000*60*5)
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        `)
-    tomato_num += 1
-    while (true) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showArrow(ArrowNames.South)
-    }
+input.onGesture(Gesture.Shake, function () {
+    basic.showString("" + (tomato_num))
+    basic.showString("" + (working))
+    basic.showString("" + (funing))
 })
 input.onButtonPressed(Button.AB, function () {
-    basic.clearScreen()
-    basic.showString("" + (tomato_num))
-})
-input.onButtonPressed(Button.B, function () {
-    basic.clearScreen()
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(1000*60*1)
-    basic.showLeds(`
-        # # . # #
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(1000*60*1)
-    basic.showLeds(`
-        # # . # #
-        # # . # #
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(1000*60*1)
-    basic.showLeds(`
-        # # . # #
-        # # . # #
-        # # . # #
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(1000*60*1)
-    basic.showLeds(`
-        # # . # #
-        # # . # #
-        # # . # #
-        # # . # #
-        . . . . .
-        `)
-    basic.pause(1000*60*1)
-    basic.showLeds(`
-        # # . # #
-        # # . # #
-        # # . # #
-        # # . # #
-        # # . # #
-        `)
-    while (true) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
+    if (working == 0 && funing == 0) {
+        working = 1
+        funing = 0
+        basic.showIcon(IconNames.SmallHeart)
+        basic.showIcon(IconNames.Heart)
         basic.showLeds(`
             . . . . .
             . . . . .
@@ -134,19 +16,108 @@ input.onButtonPressed(Button.B, function () {
             . . . . .
             . . . . .
             `)
-        basic.showArrow(ArrowNames.North)
+        basic.pause(1000*60*5)
+        basic.showLeds(`
+            # # # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000*60*5)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000*60*5)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000*60*5)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            . . . . .
+            `)
+        basic.pause(1000*60*5)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        for (let index = 0; index < 5; index++) {
+            basic.clearScreen()
+            basic.showArrow(ArrowNames.South)
+        }
+        tomato_num += 1
+        basic.showString("funing")
+        working = 0
+        funing = 1
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000*60*1)
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000*60*1)
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000*60*1)
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . .
+            `)
+        basic.pause(1000*60*1)
+        basic.showLeds(`
+            # . . . .
+            . # . . .
+            . . # . .
+            . . . # .
+            . . . . #
+            `)
+        basic.pause(1000*60*5)
+        for (let index = 0; index < 5; index++) {
+            basic.clearScreen()
+            basic.showArrow(ArrowNames.North)
+        }
+        working = 0
+        funing = 0
+    } else {
+        basic.showIcon(IconNames.No)
     }
 })
+let funing = 0
+let working = 0
 let tomato_num = 0
 tomato_num = 0
-basic.showLeds(`
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    . . . . .
-    `)
+working = 0
+funing = 0
 basic.showIcon(IconNames.Happy)
-basic.forever(function () {
-	
-})
