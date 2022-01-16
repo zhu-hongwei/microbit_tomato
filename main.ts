ae09da0926d2d474e825484b77a3f36ca3e6a798
hongweizhu.com <root@hongweizhu.com>
2022-01-16 11:53:56 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,128 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(300000)
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(300000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(300000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(300000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(300000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    for (let index = 0; index < 100; index++) {
+        basic.showArrow(ArrowNames.North)
+        basic.showIcon(IconNames.Yes)
+    }
+    tomato_num += 1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("" + (tomato_num))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(300000)
+    basic.showLeds(`
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(300000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(300000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(300000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        . . . . .
+        `)
+    basic.pause(300000)
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+    for (let index = 0; index < 100; index++) {
+        basic.showArrow(ArrowNames.North)
+        basic.showIcon(IconNames.Yes)
+    }
+    tomato_num += 1
+    basic.clearScreen()
+})
+let tomato_num = 0
+tomato_num = 0
+while (true) {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.Happy)
+}
+basic.forever(function () {
+	
+})
