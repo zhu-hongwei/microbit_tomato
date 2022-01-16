6fde26f6b849947bf67569d001b4735f0e55a1b7
hongweizhu.com <root@hongweizhu.com>
2022-01-16 12:18:29 +0800

    s

--- main.ts
+++ main.ts
@@ -47,82 +47,74 @@ input.onButtonPressed(Button.A, function () {
         # # # # #
         # # # # #
         `)
-    for (let index = 0; index < 100; index++) {
-        basic.showArrow(ArrowNames.North)
-        basic.showIcon(IconNames.Yes)
-    }
+    basic.showArrow(ArrowNames.South)
     tomato_num += 1
 })
 input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
     basic.showString("" + (tomato_num))
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(300000)
-    basic.showLeds(`
-        # # # # #
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(300000)
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(300000)
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(300000)
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        . . . . .
-        `)
-    basic.pause(300000)
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        `)
-    for (let index = 0; index < 100; index++) {
-        basic.showArrow(ArrowNames.North)
-        basic.showIcon(IconNames.Yes)
-    }
-    tomato_num += 1
     basic.clearScreen()
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(1000*60*5)
+    basic.showLeds(`
+        # # . # #
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(300000)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(300000)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        # # . # #
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(300000)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        # # . # #
+        # # . # #
+        . . . . .
+        `)
+    basic.pause(300000)
+    basic.showLeds(`
+        # # . # #
+        # # . # #
+        # # . # #
+        # # . # #
+        # # . # #
+        `)
+    basic.showArrow(ArrowNames.North)
 })
 let tomato_num = 0
 tomato_num = 0
-while (true) {
-    basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.showIcon(IconNames.Happy)
-}
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
 	
 })
