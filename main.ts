18f96760bf937a4ce80a246017e61376ff3f31a5
hongweizhu.com <root@hongweizhu.com>
2022-02-25 18:24:02 +0800

    s

--- main.ts
+++ main.ts
@@ -11,22 +11,22 @@ input.onButtonPressed(Button.A, function () {
         `)
     basic.pause(1000*60*10)
     basic.showArrow(ArrowNames.South)
-    tomato_num += 1
+    tomato_num += 10
     for (let index = 0; index < 3; index++) {
         soundExpression.spring.play()
     }
     basic.showLeds(`
         # . . . .
-        . . . . .
+        # . . . .
         . . . . .
         . . . . .
         . . . . .
         `)
-    basic.pause(1000*60*1)
-    basic.showArrow(ArrowNames.North)
-    for (let index = 0; index < 10; index++) {
-        soundExpression.twinkle.play()
+    basic.pause(1000*60*2)
+    for (let index = 0; index < 3; index++) {
+        soundExpression.spring.play()
     }
+    basic.showArrow(ArrowNames.North)
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showString("" + (tomato_num))
@@ -41,7 +41,7 @@ input.onButtonPressed(Button.B, function () {
         `)
     basic.pause(1000*60*20)
     basic.showArrow(ArrowNames.South)
-    tomato_num += 1
+    tomato_num += 20
     for (let index = 0; index < 3; index++) {
         soundExpression.spring.play()
     }
@@ -49,14 +49,14 @@ input.onButtonPressed(Button.B, function () {
         # . . . .
         # . . . .
         # . . . .
-        . . . . .
+        # . . . .
         . . . . .
         `)
-    basic.pause(1000*60*3)
-    basic.showArrow(ArrowNames.North)
-    for (let index = 0; index < 10; index++) {
-        soundExpression.twinkle.play()
+    basic.pause(1000*60*4)
+    for (let index = 0; index < 3; index++) {
+        soundExpression.spring.play()
     }
+    basic.showArrow(ArrowNames.North)
 })
 let tomato_num = 0
 tomato_num = 0
