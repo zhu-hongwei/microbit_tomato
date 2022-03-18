6bd3b51d82cd02abedc2a05ae4014fb7bce391a9
hongweizhu.com <root@hongweizhu.com>
2022-03-18 16:02:54 +0800

    1

--- main.ts
+++ main.ts
@@ -1,60 +1,95 @@
-input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    for (let index = 0; index < 3; index++) {
-        basic.showIcon(IconNames.SmallHeart)
-        basic.showIcon(IconNames.Heart)
+input.onButtonPressed(Button.A, function () {
+    if (working == 0 && funing == 0) {
+        working = 1
+        funing = 0
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000*60*10)
+        basic.showArrow(ArrowNames.South)
+        mins += 10
+        working = 0
+        funing = 1
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.pause(1000*60*2)
+        basic.showArrow(ArrowNames.North)
+        working = 0
+        funing = 0
+    } else {
+        basic.showIcon(IconNames.No)
+        tip()
     }
 })
-input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(1000*60*10)
-    basic.showArrow(ArrowNames.South)
-    tomato_num += 10
-    basic.showLeds(`
-        # . . . .
-        # . . . .
-        . . . . .
-        . . . . .
-        . . . . .
-        `)
-    basic.pause(1000*60*2)
-    basic.showArrow(ArrowNames.North)
-})
+function tip () {
+    if (working == 1) {
+        basic.showString("working")
+    } else if (funing == 1) {
+        basic.showString("funing")
+    } else {
+    	
+    }
+}
 input.onGesture(Gesture.Shake, function () {
-    basic.clearScreen()
-    basic.showString("" + (tomato_num))
+    if (working == 0 && funing == 0) {
+        basic.clearScreen()
+        basic.showString("" + (mins))
+    } else {
+        tip()
+    }
 })
 input.onButtonPressed(Button.AB, function () {
-    tomato_num += 10
+    if (working == 0 && funing == 0) {
+        mins += 10
+    } else {
+        tip()
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        # # # # #
-        # # # # #
-        # # # # #
-        # # # # #
-        . . . . .
-        `)
-    basic.pause(1000*60*20)
-    basic.showArrow(ArrowNames.South)
-    tomato_num += 20
-    basic.showLeds(`
-        # . # . .
-        # . . . .
-        # . . . .
-        # . . . .
-        . . . . .
-        `)
-    basic.pause(1000*60*4)
-    basic.showArrow(ArrowNames.North)
+    if (working == 0 && funing == 0) {
+        working = 1
+        funing = 0
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            . . . . .
+            `)
+        basic.pause(1000*60*20)
+        basic.showArrow(ArrowNames.South)
+        mins += 20
+        working = 0
+        funing = 1
+        basic.showLeds(`
+            # . . . .
+            # . . . .
+            # . . . .
+            # . . . .
+            . . . . .
+            `)
+        basic.pause(1000*60*4)
+        basic.showArrow(ArrowNames.North)
+        working = 0
+        funing = 0
+    } else {
+        basic.showIcon(IconNames.No)
+        tip()
+    }
 })
-let tomato_num = 0
-tomato_num = 0
-let working = 0
+let mins = 0
 let funing = 0
+let working = 0
+working = 0
+funing = 0
+mins = 0
 basic.showIcon(IconNames.Happy)
