d7fec1c0b41e2f0059677caa21e7f5b851f48126
hongweizhu.com <root@hongweizhu.com>
2022-03-21 16:35:25 +0800

    sound

--- main.ts
+++ main.ts
@@ -11,6 +11,7 @@ input.onButtonPressed(Button.A, function () {
             `)
         basic.pause(1000*60*10)
         basic.showArrow(ArrowNames.South)
+        music.playMelody("C D E F G A B C5 ", 120)
         mins += 10
         working = 0
         funing = 1
@@ -23,6 +24,7 @@ input.onButtonPressed(Button.A, function () {
             `)
         basic.pause(1000*60*2)
         basic.showArrow(ArrowNames.North)
+        music.playMelody("C D E F G A B C5 ", 120)
         working = 0
         funing = 0
     } else {
@@ -67,6 +69,7 @@ input.onButtonPressed(Button.B, function () {
             `)
         basic.pause(1000*60*20)
         basic.showArrow(ArrowNames.South)
+        music.playMelody("C D E F G A B C5 ", 120)
         mins += 20
         working = 0
         funing = 1
@@ -79,6 +82,7 @@ input.onButtonPressed(Button.B, function () {
             `)
         basic.pause(1000*60*4)
         basic.showArrow(ArrowNames.North)
+        music.playMelody("C D E F G A B C5 ", 120)
         working = 0
         funing = 0
     } else {
@@ -93,3 +97,4 @@ working = 0
 funing = 0
 mins = 0
 basic.showIcon(IconNames.Happy)
+music.playMelody("C D E F G A B C5 ", 120)
